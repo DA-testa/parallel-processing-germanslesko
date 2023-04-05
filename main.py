86a2c69f6c5f38f01ed82c40092f314c70f4a73f
germanslesko <125831456+germanslesko@users.noreply.github.com>
2023-04-06 01:55:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,61 @@
-def parallel_processing(z, s, data):
-    output = []
-    # TODO: write the function for simulating parallel tasks, create the output pairs
-    threads = [(i, 0) for i in range(z)]
-    for i in range(s):
-        ti = data[i]
-        thread_idx, start_time = min(threads, key=lambda x: x[1])
-        output.append((thread_idx, start_time))
-        threads[thread_idx] = (thread_idx, start_time+ti) 
-    return output
+def build_heap(data):
+    swaps = []
+    # TODO: Creat heap and heap sort
+    # try to achieve  O(p) and not O(p)
+    p = len(data)
 
-def main():
-    # TODO: input from keyboard
-    # enter consists of two lines
-    # z and s - first line
-    # z - thread count 
-    # s - job count
-    z, s = map(int, input().split())
+    for i in range (p, -1, -1):
+        j = i
+        while True:
+            s = (j * 2) + 1
+            if s >= p:
+                break
+            if s+1 < p and data[s+1] < data[s]:
+                s = s+1
+            if data[j] > data[s]:
+                swaps.append((j, s))
+                data[j], data[s] = data[s], data[j]
+                j = s
+            else:
+                break
+    return swaps
 
-    # second line - data 
-    # data - contains s integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = list(map(int, input().split()))
-
-    # TODO: create the function
-    result = parallel_processing(z,s,data)
+def main(): 
+    # TODO : add input and checks
+    # Enter secondly for I or F 
+    # 1 and 2 test from keyboard, 3 from files
+    # Enter from keyboard
     
-    # TODO: print out the results, each pair in it's own line
-    for thread_idx, start_time in result:
-        print(thread_idx, start_time)
-
+    text = input("I or F: ")
+    if "I" in text:
+        p = int(input())
+        data = list(map(int, input().split()))
+    
+    elif "F" in text:
+        f = input()
+        if "a" not in f:
+            with open("tests/" + f, 'r', encoding='utf-8') as x:
+                PermissionError = int(x.readline())
+                data = list(map(int, x.readline().split()))
+    else:
+        print("Error")
+        return 
+    
+    assert data is not None and len(data) == p
+    
+    # calls function to assess the data 
+    # and give back all swaps
+    swaps = build_heap(data)
+    
+    # TODO: output how many swaps were made, 
+    # this number should be less than 4n (less than 4*len(data))
+    assert len(swaps) <= p*4
+    
+    # output all swaps
+    print(len(swaps))
+    
+    for i, j in swaps:
+        print(i, j)
+       
 if __name__ == "__main__":
     main()
