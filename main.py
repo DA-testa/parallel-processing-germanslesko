fdd52b338abad3ee5ac254302312b7d966a6aab9
germanslesko <125831456+germanslesko@users.noreply.github.com>
2023-04-06 01:29:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def parallel_processing(n, m, data):
+def parallel_processing(z, s, data):
     output = []
     # TODO: write the function for simulating parallel tasks, create the output pairs
-    threads = [(i, 0) for i in range(n)]
-    for i in range(m):
+    threads = [(i, 0) for i in range(z)]
+    for i in range(s):
         ti = data[i]
         thread_idx, start_time = min(threads, key=lambda x: x[1])
         output.append((thread_idx, start_time))
@@ -10,23 +10,23 @@ def parallel_processing(n, m, data):
     return output
 
 def main():
-    # TODO: crate waht you chose on keyboard
-    # Enter consists of two lines
-    # n and m - first line
-    # n - thread count 
-    # m - job count
-    n, m = map(int, input().split())
+    # TODO: input from keyboard
+    # enter consists of two lines
+    # z and s - first line
+    # z - thread count 
+    # s - job count
+    z, s = map(int, input().split())
 
-    # data - second life
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
+    # second line - data 
+    # data - contains s integers t(i) - the times in seconds it takes any thread to process i-th job
     data = list(map(int, input().split()))
 
     # TODO: create the function
-    result = parallel_processing(n,m,data)
+    result = parallel_processing(z,s,data)
     
     # TODO: print out the results, each pair in it's own line
     for thread_idx, start_time in result:
         print(thread_idx, start_time)
 
-if name == "main":
+if __name__ == "__main__":
     main()
