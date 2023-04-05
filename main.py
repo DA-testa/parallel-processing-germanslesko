07f5a842cbd1e81c54be5cb59dd897c169c54180
germanslesko <125831456+germanslesko@users.noreply.github.com>
2023-04-06 01:10:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-# python3
+# Germans Lesko 221RDB464 15.grupa
 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
-
+    # TODO: write the function for simulating parallel tasks, create the output pairs
+    threads = [(i, 0) for i in range(n)]
+    for i in range(m):
+        ti = data[i]
+        thread_idx, start_time = min(threads, key=lambda x: x[1])
+        output.append((thread_idx, start_time))
+        threads[thread_idx] = (thread_idx, start_time+ti) 
     return output
 
 def main():
@@ -13,19 +17,18 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    n, m = map(int, input().split())
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    data = list(map(int, input().split()))
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
     
     # TODO: print out the results, each pair in it's own line
+    for thread_idx, start_time in result:
+        print(thread_idx, start_time)
 
-
-
-if __name__ == "__main__":
+if name == "main":
     main()
